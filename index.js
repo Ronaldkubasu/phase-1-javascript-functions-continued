dec2bb9442d84d129f1f832d8b283d82209daca7
Ronald kubasu <ronald.kubasu@student.moringaschool.com>
2022-08-19 13:45:08 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(args = 'roller-skate'){
+    return(`This Saturday, I want to ${args}!`)
+}
+function mondayWork(args = 'go to the office'){
+    return(`This Monday, I will ${args}.`)
+}
+function wrapAdjective(flair = '*'){
+    return function(arg = 'special'){
+        return `You are ${flair}${arg}${flair}!`
+    }
+}
+console.log(wrapAdjective()())
